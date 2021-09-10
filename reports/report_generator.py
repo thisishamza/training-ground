f6f5548d79f78785ea730aa24023cf65fded98cf
mbilalhameed <mbilal.hameed@outlook.com>
2021-09-10 18:46:05 +0500

    Formated code according to pep8. Modified print statements into f-strings

--- reports/report_generator.py
+++ reports/report_generator.py
@@ -1,34 +1,39 @@
-def chart_on_month(data):
+def bar_charts_for_temperature(readings_data_structure, date):
     """generates a bar chart of maximum and minimum temperature on each day of a month
 
     Args:
         data ([Dictionary]): [Nested dictionary of all data]
     """
-    year = input("Enter Year for Bar-Chart as YYYY ")
-    month = input("Enter Month for Bar-Chart as MM ")
+    date = date.split('/')
+    year = date[0]
+    month = date[1]
     if int(month) < 10:
         month = month[1]
     CRED = '\33[31m'
     CBLUE = '\33[34m'
     CWHITE = '\33[37m'
-    for date_key in data.keys():
+    for date_key in readings_data_structure.keys():
         if date_key.split('-')[0] == year and date_key.split('-')[1] == month:
-            max_temp = int(data[date_key]['Max TemperatureC'])
-            min_temp = int(data[date_key]['Min TemperatureC'])
-            print(date_key.split('-')[-1], CRED + "+"*max_temp + CWHITE, max_temp, "C")
-            print(date_key.split('-')[-1], CBLUE + "+"*min_temp + CWHITE, min_temp, "C\n")
+            max_temp = int(readings_data_structure[date_key]['Max TemperatureC'])
+            min_temp = int(readings_data_structure[date_key]['Min TemperatureC'])
+            print(f"{date_key.split('-')[-1]} {CRED}",
+                  '+'*max_temp, f"{CWHITE} {max_temp} C")
+            print(f"{date_key.split('-')[-1]} {CBLUE}",
+                  '+'*min_temp, f"{CWHITE} {min_temp} C")
 
 
-def chart_on_month_bonus_task(data):
-    year = input("Enter Year for Bar-Chart as YYYY ")
-    month = input("Enter Month for Bar-Chart as MM ")
+def single_bar_chart_temperature(readings_data_structure, date):
+    date = date.split('/')
+    year = date[0]
+    month = date[1]
     if int(month) < 10:
         month = month[1]
     CRED = '\33[31m'
     CBLUE = '\33[34m'
     CWHITE = '\33[37m'
-    for date_key in data.keys():
+    for date_key in readings_data_structure.keys():
         if date_key.split('-')[0] == year and date_key.split('-')[1] == month:
-            max_temp = int(data[date_key]['Max TemperatureC'])
-            min_temp = int(data[date_key]['Min TemperatureC'])
-            print(date_key.split('-')[-1], CBLUE + "+"*min_temp + CRED + "+"*max_temp + CWHITE, min_temp, "-", max_temp)
+            max_temp = int(readings_data_structure[date_key]['Max TemperatureC'])
+            min_temp = int(readings_data_structure[date_key]['Min TemperatureC'])
+            print(date_key.split('-')[-1], CBLUE + "+"*min_temp +
+                  CRED + "+"*max_temp + CWHITE, min_temp, "-", max_temp)
