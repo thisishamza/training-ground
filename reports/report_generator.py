f946117a3a72f86af83be3615390d0e3a6798bc1
mbilalhameed <mbilal.hameed@outlook.com>
2021-09-15 12:20:53 +0500

    changed structure of reading data, modified calculations and report generation

--- reports/report_generator.py
+++ reports/report_generator.py
@@ -1,39 +1,30 @@
-def bar_charts_for_temperature(readings_data_structure, date):
+def bar_charts_for_temperature(weather_readings):
     """generates a bar chart of maximum and minimum temperature on each day of a month
 
     Args:
-        data ([Dictionary]): [Nested dictionary of all data]
+        weather_readings ([List]): [Nested list of given month data]
     """
-    date = date.split('/')
-    year = date[0]
-    month = date[1]
-    if int(month) < 10:
-        month = month[1]
     CRED = '\33[31m'
     CBLUE = '\33[34m'
     CWHITE = '\33[37m'
-    for date_key in readings_data_structure.keys():
-        if date_key.split('-')[0] == year and date_key.split('-')[1] == month:
-            max_temp = int(readings_data_structure[date_key]['Max TemperatureC'])
-            min_temp = int(readings_data_structure[date_key]['Min TemperatureC'])
-            print(f"{date_key.split('-')[-1]} {CRED}",
-                  '+'*max_temp, f"{CWHITE} {max_temp} C")
-            print(f"{date_key.split('-')[-1]} {CBLUE}",
-                  '+'*min_temp, f"{CWHITE} {min_temp} C")
+    min_temperature = weather_readings[1]
+    max_temperature = weather_readings[0]
+    for day, weather_reading in enumerate(weather_readings[0]):
+        print(f"{day+1} {CRED}", '+' * int(max_temperature[day]), f"{CWHITE} {max_temperature[day]}C")
+        print(f"{day+1} {CBLUE}", '+' * int(min_temperature[day]), f"{CWHITE} {min_temperature[day]}C")
 
 
-def single_bar_chart_temperature(readings_data_structure, date):
-    date = date.split('/')
-    year = date[0]
-    month = date[1]
-    if int(month) < 10:
-        month = month[1]
+def single_bar_chart_temperature(weather_readings):
+    """generates a bar chart of maximum and minimum temperature on each day of a month
+
+    Args:
+        weather_readings ([List]): [Nested list of given month data]
+    """
     CRED = '\33[31m'
     CBLUE = '\33[34m'
     CWHITE = '\33[37m'
-    for date_key in readings_data_structure.keys():
-        if date_key.split('-')[0] == year and date_key.split('-')[1] == month:
-            max_temp = int(readings_data_structure[date_key]['Max TemperatureC'])
-            min_temp = int(readings_data_structure[date_key]['Min TemperatureC'])
-            print(date_key.split('-')[-1], CBLUE + "+"*min_temp +
-                  CRED + "+"*max_temp + CWHITE, min_temp, "-", max_temp)
+    min_temperature = weather_readings[1]
+    max_temperature = weather_readings[0]
+    for day, weather_reading in enumerate(weather_readings[0]):
+        print(day+1, CBLUE + "+"*int(min_temperature[day]) + CRED + "+"*int(max_temperature[day]) + CWHITE, 
+              min_temperature[day], "-", max_temperature[day])
--- weather_man.py
+++ weather_man.py
@@ -1,12 +1,15 @@
-import os
-import glob
-import csv
 import argparse
-from calculations import do_calculations
+import csv
+import glob
+import os
+
+from datetime import datetime
+
+from calculations import temperature_calculations
 from reports import report_generator
 
-readings_data_structure = {}
-path_to_files = os.getcwd() + '/weatherfiles'
+
+complete_weather_readings = {}
 
 
 def to_correct_type(value):
@@ -29,58 +32,125 @@ def to_correct_type(value):
             return value
 
 
-def populate_readings_data_structure(single_day_reading):
+def convert_date_to_year_month_day(date):
+    date = date.split('/')
+    if len(date) > 0:
+        year = date[0]
+    else:
+        year = ""
+    if len(date) > 1:
+        month = date[1]
+        if int(month) < 10:
+            try:
+                month = month[1]
+            except IndexError:
+                month = month[0]
+    else:
+        month = ""
+    if len(date) > 2:
+        day = date[-1]
+    else:
+        day = " "
+    return {
+        'year': year,
+        'month': month,
+        'day': day
+    }
+
+
+def get_date_from_daily_weather_reading(daily_weather_reading):
+    try:
+        return datetime.strptime(str(daily_weather_reading['PKT']), "%Y-%m-%d")
+    except KeyError:
+        return datetime.strptime(str(daily_weather_reading['PKST']), "%Y-%m-%d")
+
+
+def populate_weather_readings(single_day_reading, date):
     """receives a single line from reading file and generates a dictionary of values
 
     Args:
-        split_line ([String]): [A single line read from file]
+        single_day_reading ([Dictionary]): [A dictionary having single day weather reading]
+        date ([datetime]): [A datetime object containing date of every single row]
 
     Returns:
-        [Dictionary]: [A dictionary containing datapoints for a tuple]
+        [Dictionary]: [A dictionary containing datapoints required by the task]
     """
-    single_day_reading['Max TemperatureC'] = to_correct_type(
+    day_reading = {}
+    day_reading['date'] = date
+    day_reading['Max TemperatureC'] = to_correct_type(
         single_day_reading['Max TemperatureC'])
-    single_day_reading['Mean TemperatureC'] = to_correct_type(
+    day_reading['Mean TemperatureC'] = to_correct_type(
         single_day_reading['Mean TemperatureC'])
-    single_day_reading['Min TemperatureC'] = to_correct_type(
+    day_reading['Min TemperatureC'] = to_correct_type(
         single_day_reading['Min TemperatureC'])
-    single_day_reading['Dew PointC'] = to_correct_type(
-        single_day_reading['Dew PointC'])
-    single_day_reading['MeanDew PointC'] = to_correct_type(
-        single_day_reading['MeanDew PointC'])
-    single_day_reading['Min DewpointC'] = to_correct_type(
-        single_day_reading['Min DewpointC'])
-    single_day_reading['Max Humidity'] = to_correct_type(
+    day_reading['Max Humidity'] = to_correct_type(
         single_day_reading['Max Humidity'])
-    single_day_reading['Mean Humidity'] = to_correct_type(
+    day_reading['Mean Humidity'] = to_correct_type(
         single_day_reading[' Mean Humidity'])
-    single_day_reading['Min Humidity'] = to_correct_type(
+    day_reading['Min Humidity'] = to_correct_type(
         single_day_reading[' Min Humidity'])
-    single_day_reading['CloudCover'] = to_correct_type(
-        single_day_reading[' CloudCover'])
-    single_day_reading['WindDirDegrees'] = to_correct_type(
-        single_day_reading['WindDirDegrees'])
-    single_day_reading['Max VisibilityKm'] = to_correct_type(
-        single_day_reading[' Max VisibilityKm'])
-    single_day_reading['Mean VisibilityKm'] = to_correct_type(
-        single_day_reading[' Mean VisibilityKm'])
-    single_day_reading['Min VisibilitykM'] = to_correct_type(
-        single_day_reading[' Min VisibilitykM'])
-    single_day_reading['Max Wind SpeedKm/h'] = to_correct_type(
-        single_day_reading[' Max Wind SpeedKm/h'])
-    single_day_reading['Mean Wind SpeedKm/h'] = to_correct_type(
-        single_day_reading[' Mean Wind SpeedKm/h'])
-    single_day_reading['Max Gust SpeedKm/h'] = to_correct_type(
-        single_day_reading[' Max Gust SpeedKm/h'])
-    single_day_reading['Precipitationmm'] = to_correct_type(
-        single_day_reading['Precipitationmm'])
-    single_day_reading['Max Sea Level PressurehPa'] = to_correct_type(
-        single_day_reading[' Max Sea Level PressurehPa'])
-    single_day_reading['Mean Sea Level PressurehPa'] = to_correct_type(
-        single_day_reading[' Mean Sea Level PressurehPa'])
-    single_day_reading['Min Sea Level PressurehPa'] = to_correct_type(
-        single_day_reading[' Min Sea Level PressurehPa'])
-    return single_day_reading
+    return day_reading
+
+
+def compile_data_for_monthly_reports(date):
+    maximum_temperature = {}
+    minimum_temperature = {}
+    for key_year, year in complete_weather_readings.items():
+        for key_month, month in year.items():
+            maximum_temperature.setdefault(
+                key_year, {}).setdefault(
+                    key_month, [daily_readings['Max TemperatureC'] for daily_readings in month])
+            minimum_temperature.setdefault(
+                key_year, {}).setdefault(
+                    key_month, [daily_readings['Min TemperatureC'] for daily_readings in month])
+    year = int(date['year'])
+    month = int(date['month'])
+    yearly_maximum_reading = maximum_temperature[year]
+    yearly_minimum_reading = minimum_temperature[year]
+    return [yearly_maximum_reading[month], yearly_minimum_reading[month]]
+
+
+def compile_data_for_monthly_calculations():
+    avg_minimum_temperature_every_month = {}
+    avg_maximum_temperature_every_month = {}
+    avg_mean_humidity_every_month = {}
+    for key_year, year in complete_weather_readings.items():
+        for key_month, month in year.items():
+            avg_maximum_temperature_every_month.setdefault(
+                key_year, {}).setdefault(key_month, float(
+                    sum(daily_readings['Max TemperatureC'] for daily_readings in month)) / len(month))
+            avg_minimum_temperature_every_month.setdefault(
+                key_year, {}).setdefault(key_month, float(
+                    sum(d['Min TemperatureC'] for d in month)) / len(month))
+            avg_mean_humidity_every_month.setdefault(
+                key_year, {}).setdefault(key_month, float(
+                    sum(d['Mean Humidity'] for d in month)) / len(month))
+    return [avg_maximum_temperature_every_month, 
+            avg_minimum_temperature_every_month, 
+            avg_mean_humidity_every_month]
+
+
+def compile_data_for_yearly_calculations():
+    minimum_temperature_every_month = {}
+    maximum_temperature_every_month = {}
+    maximum_humidity_every_month = {}
+    for key_year, year in complete_weather_readings.items():
+        minimum_temperature = []
+        maximum_temperature = []
+        maximum_humidity = []
+        for key_month, month in year.items():
+            minimum_temperature.append(
+                min(month, key=lambda x: x['Min TemperatureC']))
+            minimum_temperature_every_month[key_year] = minimum_temperature
+            maximum_temperature.append(
+                max(month, key=lambda x: x['Max TemperatureC']))
+            maximum_temperature_every_month[key_year] = maximum_temperature
+            maximum_humidity.append(
+                max(month, key=lambda x: x['Mean Humidity']))
+            maximum_humidity_every_month[key_year] = maximum_humidity
+    return [minimum_temperature_every_month, 
+            maximum_temperature_every_month, 
+            maximum_humidity_every_month]
 
 
 def read_data(path_to_files):
@@ -91,19 +161,32 @@ def read_data(path_to_files):
     """
     for file_name in glob.glob(os.path.join(path_to_files, '*.txt')):
         with open(file_name, mode='r') as csv_file:
-            single_file_table = csv.DictReader(csv_file)
-            for single_day_reading in single_file_table:
-                if single_day_reading:
-                    single_day_reading = populate_readings_data_structure(
-                        single_day_reading)
-                    try:
-                        date = single_day_reading['PKT']
-                    except KeyError:
-                        date = single_day_reading['PKST']
-                    readings_data_structure[date] = single_day_reading
+            for single_day_weather_reading in csv.DictReader(csv_file):
+                if single_day_weather_reading:
+                    if single_day_weather_reading.get('PKT'):
+                        date = datetime.strptime(
+                            str(single_day_weather_reading['PKT']), "%Y-%m-%d")
+                    else:
+                        date = datetime.strptime(
+                            str(single_day_weather_reading['PKST']), "%Y-%m-%d")
+                    day_reading = populate_weather_readings(
+                        single_day_weather_reading, date)
+                    complete_weather_readings.setdefault(date.year, {}).setdefault(
+                        date.month, []).append(day_reading)
 
 
 def valid_directory_path(directory_path):
+    """Checks if a path to a directory is valid or not
+
+    Args:
+        directory_path ([String]): [Path to directory containing data files]
+
+    Raises:
+        argparse.ArgumentTypeError: [TypeError if path to directory is invalid]
+
+    Returns:
+        [argparse.Namespace]: [description]
+    """
     if os.path.isdir(directory_path):
         return directory_path
     else:
@@ -124,18 +207,20 @@ def parse_arguments():
 def main():
     parsed_arguments = parse_arguments()
     arguments = vars(parsed_arguments)
-    read_data(path_to_files=arguments['path'])
+    read_data(arguments['path'])
     if arguments['e']:
-        do_calculations.highest_lowest_temperature(
-            readings_data_structure, arguments['e'])
+        yearly_weather_readings = compile_data_for_yearly_calculations()
+        temperature_calculations.highest_lowest_temperature(
+            yearly_weather_readings, arguments['e'])
     if arguments['a']:
-        do_calculations.average_highest_lowest_temperature(
-            readings_data_structure, arguments['a'])
+        monthly_weather_readings = compile_data_for_monthly_calculations()
+        temperature_calculations.average_highest_lowest_temperature(
+            monthly_weather_readings, convert_date_to_year_month_day(arguments['a']))
     if arguments['c']:
-        report_generator.bar_charts_for_temperature(
-            readings_data_structure, arguments['c'])
-        report_generator.single_bar_chart_temperature(
-            readings_data_structure, arguments['c'])
+        monthly_report_readings = compile_data_for_monthly_reports(
+            convert_date_to_year_month_day(arguments['c']))
+        report_generator.bar_charts_for_temperature(monthly_report_readings)
+        report_generator.single_bar_chart_temperature(monthly_report_readings)
 
 
 if __name__ == "__main__":
